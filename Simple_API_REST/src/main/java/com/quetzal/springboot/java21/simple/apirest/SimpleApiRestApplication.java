a6c2f13a6dfda531458d5a773bb9eca18dcec392
Diego Hernandez Cote <quetzal.developer@gmail.com>
2025-01-09 15:22:33 -0600

    Add. Project: Simple_API_REST. Creation of project

---
+++ Simple_API_REST/src/main/java/com/quetzal/springboot/java21/simple/apirest/SimpleApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.quetzal.springboot.java21.simple.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiRestApplication.class, args);
+	}
+
+}
